6fa25bae2130573002c1ae8c06ab9ae4ebb7606a
w87777w <49546405+w87777w@users.noreply.github.com>
2020-09-30 17:27:14 +0800

    Update embyonekey.sh

--- embyonekey.sh
+++ embyonekey.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /var/packages/EmbyServer/target/mono/bin
-curl https://raw.githubusercontent.com/s1oz/embyonekey/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+curl https://raw.githubusercontent.com/w87777w/embyonekey/master/20mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
 ./cert-sync /etc/ssl/certs/ca-certificates.crt
 cd /usr/local/etc/nginx/conf.d/*-*-*-*
 wget https://raw.githubusercontent.com/s1oz/embyonekey/master/user.conf
